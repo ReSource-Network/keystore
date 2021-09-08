a879515006bda3e785cc6ae8f40277650a0dc996
Sayer <sayer@worldtree.io>
2021-09-07 17:30:53 -0700

    cleanup

--- src/server.ts
+++ src/server.ts
@@ -18,10 +18,7 @@ export const createServer = (
   // cors headers
   app.use(function (req, res, next) {
     res.set("Access-Control-Allow-Origin", "*");
-    res.set(
-      "Access-Control-Allow-Methods",
-      "GET, POST, OPTIONS, PUT, PATCH, DELETE",
-    );
+    res.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
     res.set("Access-Control-Allow-Headers", "X-Requested-With,content-type");
 
     next();
